ff5cae4b2c300965101ebbd4c75da1763f77357b
lantanum <119154526+lantanum@users.noreply.github.com>
2023-10-18 00:07:49 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
-python3.11.5 manage.py collectstatic --clear
-python3.11.5 manage.py collectstatic
+python manage.py collectstatic --clear
+python manage.py collectstatic
