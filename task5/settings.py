8ac5c4d4eadc74e2f3c1f31d5d2447d54290b108
Akbar <geraldrosentalie@gmail.com>
2023-10-18 01:32:24 +0600

    m

--- task5/settings.py
+++ task5/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
